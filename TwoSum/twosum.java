bfdc1895d03d7370a0bef03973acc64bcc23c2ef
Johann Soetbeer <johann.soetbeer@outlook.com>
2026-03-27 13:05:00 +0100

    updated solution for two Sum problem

--- TwoSum/twosum.java
+++ TwoSum/twosum.java
@@ -1,21 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int sum=0;
-        int temp1=0;
-        int temp2=0;
-        for (int i=0; i<nums.length-1; i++) {
-            for (int j=i+1; j<nums.length; j++){
-                sum = nums[i] + nums[j];
-                if (sum == target) {
-                    temp1=i;
-                    temp2=j;
-                    break;
+        for (int i = 0; i < nums.length -1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i,j};
                 }
             }
-
         }
-
-        int [] result = {temp1,temp2};
-        return result;
+        return null;
     }
 }
