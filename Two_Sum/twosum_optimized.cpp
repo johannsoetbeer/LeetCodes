950d70a8487ce2498265c09c8e0a67f9ccbc2d88
Johann Soetbeer <johann.soetbeer@gmail.com>
2026-08-31 09:09:27 +0200

    created new solution for "Two Sum " in O(N) using hshmaps

---
+++ Two_Sum/twosum_optimized.cpp
@@ -0,0 +1,14 @@
+#include <unordered_map>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            if (map.contains(target - nums[i])) {
+                return {map[target - nums[i]],i};
+            }
+            map[nums[i]] = i;
+        }
+        return {0,0};
+    }
+};
